5fa353d43231f349a96e9133a21de1f8cc6c3d40
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2023-01-16 14:03:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require 'vendor/autoload.php';
 require 'core/bootstrap.php';
 
 use App\Core\{Router, Request};
-
+session_start();
 Router::load('app/routes.php')
     ->direct(Request::uri(), Request::method());
